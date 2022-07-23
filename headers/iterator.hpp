bfc5e9dc0b0cd3a0690b643c475237ebdf6f1500
Alice <purpleplayer@yandex.ru>
2022-07-23 18:49:23 +0300

    Add part of member functions in normal_iterator except conversion it -> const it

--- headers/iterator.hpp
+++ headers/iterator.hpp
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#include <iterator>
+#include <iterator> 
 #include <iostream> // debug
 
 namespace ft
@@ -26,7 +26,7 @@ struct iterator_traits<T*>
 	typedef T								value_type;
 	typedef T*								pointer;
 	typedef T&								reference;
-	//typedef ptrdiff_t						difference_type;
+	typedef ptrdiff_t						difference_type;
 	typedef std::random_access_iterator_tag	iterator_category;
 
 }; // struct iterator_traits<T*>
@@ -37,7 +37,7 @@ struct iterator_traits<const T*>
 	typedef T								value_type;
 	typedef const T*						pointer;
 	typedef const T&						reference;
-	//typedef ptrdiff_t						difference_type;
+	typedef ptrdiff_t						difference_type;
 	typedef std::random_access_iterator_tag	iterator_category;
 
 }; // struct iterator_traits<const T*>
@@ -72,15 +72,62 @@ class normal_iterator
 			std::cout << "normal_iterator: " << "copy constructor" << std::endl; // debug	
 		}
 
+		/* need special test
+		template<typename iter>
+		normal_iterator(const normal_iterator<it, typename enable_if<is_equal_types<it, typename container::pointer>(), container>::type>& it)
+		:	m_current_iterator(it.get_iterator())
+		{
+			std::cout << "normal_iterator: " << "iter to const_iter conversion" << std::endl; // debug
+		}
+		*/
+
 	public:
 
-		// ...
-
 		const iterator_type& get_iterator() const throw()
+		{ return m_current_iterator; }
+
+		reference operator*() const throw()
+		{ return *m_current_iterator; }
+
+		pointer operator->() const throw()
+		{ return m_current_iterator; }
+
+		normal_iterator& operator++() throw()
+		{ ++m_current_iterator; return *this; }
+
+		normal_iterator operator++(int) throw()
+		{ return normal_iterator(m_current_iterator++); }
+
+		normal_iterator& operator--() throw()
+		{ --m_current_iterator; return *this; }
+
+		normal_iterator operator--(int) throw()
+		{ return normal_iterator(m_current_iterator--); }
+
+		reference operator[](difference_type n) const throw()
+		{ return m_current_iterator[n]; }
+
+		normal_iterator& operator+=(difference_type n) throw()
+		{ return m_current_iterator += n; return *this }
+
+		normal_iterator operator+(difference_type n) const throw()
+		{ return normal_iterator(m_current_iterator + n); }
+
+		normal_iterator& operator-=(difference_type n) throw
+		{ m_current_iterator -= n; return *this; }
+
+		normal_iterator operator-(difference_type n) const throw()
+		{ return normal_iterator(m_current_iterator - n); }
+
+	private:
+
+		/* need special test
+		template<typename type_1, typename type_2> // mb move in utils.cpp
+		bool is_equal_types()
 		{
-			std::cout << "normal_iterator: " << "get_iterator()" << std::endl; // debug
-			return m_current_iterator;
+			return typeid(type_1) == typeid(type_2);
 		}
+		*/
 
 }; // class normal_iterator
 
